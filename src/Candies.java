63bc028bef3493dd5d2dc4f101df8bcfffd9969b
Saikat Maitra <saikat.maitra@gmail.com>
2012-12-11 22:17:02 +0530

    Candies changes
    Signed-off-by: Saikat Maitra <saikat.maitra@gmail.com>

--- src/Candies.java
+++ src/Candies.java
@@ -7,11 +7,11 @@ public class Candies {
 
 	public static void main(String[] args) throws Exception{
 
-		Date starttime = new Date();
-		File f =  new File("./Candies/input00.txt");
-		Scanner in = new Scanner(f);
+//		Date starttime = new Date();
+//		File f =  new File("./Candies/input01.txt");
+//		Scanner in = new Scanner(f);
 
-		//Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 		int N;
 		N = in.nextInt();
 	
@@ -20,13 +20,31 @@ public class Candies {
 		for(int i=0; i<N; i++){
 			x[i] = in.nextInt();
 		}
+		int minCandies = 1;
+		int candies = 1;
 		
-		
-		
-		Date endtime = new Date();
-		long time=endtime.getTime()-starttime.getTime();
-		System.out.println("Time taken "+time+"milisecs");
+		for(int i=1;i<N;i++){
+			//System.out.println("i "+i);
+			//System.out.println("x[i] "+x[i]);
+			
+			if(x[i]>x[i-1]){
+				minCandies=minCandies+1;
+				candies=candies+minCandies;
 				
+			}else{
+				minCandies=1;
+				candies=candies+minCandies;
+				
+			}
+			//if(minCandies<=0)
+				//System.out.println("minCandies "+minCandies);
+			//System.out.println("candies "+candies);
+		}
+		System.out.println(candies);
+//		Date endtime = new Date();
+//		long time=endtime.getTime()-starttime.getTime();
+//		System.out.println("Time taken "+time+"milisecs");
+//				
 
 		
 	}
