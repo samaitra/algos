362d9e92d898e287fd6a4d6cd5ce625798c5fbe0
Saikat Maitra <saikat.maitra@gmail.com>
2012-12-12 01:06:11 +0530

    solution

--- src/Candies.java
+++ src/Candies.java
@@ -2,51 +2,40 @@ import java.io.File;
 import java.util.Date;
 import java.util.Scanner;
 
-
 public class Candies {
 
 	public static void main(String[] args) throws Exception{
 
-//		Date starttime = new Date();
-//		File f =  new File("./Candies/input01.txt");
-//		Scanner in = new Scanner(f);
+		 int n, i, j, sum=0;
+		 File f = new File("./Candies/input01.txt");
+		 Scanner in = new Scanner(f);
+		 //Scanner in = new Scanner(System.in);
+		 
+		 n = in.nextInt(); 
+         int rating[] = new int[n];
+         int candies[] = new int[n];
+        for(i=0;i<n;i++)
+                rating[i]=in.nextInt();
+        for(i=0;i<n;i++){
+                candies[i]=1;
+                if(i>0){
+                        if(rating[i]>rating[i-1])
+                                candies[i]=candies[i-1]+1;
+                        else{
+                                j=i;
+                                while(j>0 && rating[j]<rating[j-1]){
+                                        candies[j-1]=max(candies[j-1],candies[j]+1);
+                                        j--;
+                                }
+                        }
+                }
+        }
+        for(i=0;i<n;i++)
+                sum+=candies[i];
+    System.out.println(sum);
+    }
 
-        Scanner in = new Scanner(System.in);
-		int N;
-		N = in.nextInt();
-	
-		int x[] = new int[N];
-			
-		for(int i=0; i<N; i++){
-			x[i] = in.nextInt();
-		}
-		int minCandies = 1;
-		int candies = 1;
-		
-		for(int i=1;i<N;i++){
-			//System.out.println("i "+i);
-			//System.out.println("x[i] "+x[i]);
-			
-			if(x[i]>x[i-1]){
-				minCandies=minCandies+1;
-				candies=candies+minCandies;
-				
-			}else{
-				minCandies=1;
-				candies=candies+minCandies;
-				
-			}
-			//if(minCandies<=0)
-				//System.out.println("minCandies "+minCandies);
-			//System.out.println("candies "+candies);
-		}
-		System.out.println(candies);
-//		Date endtime = new Date();
-//		long time=endtime.getTime()-starttime.getTime();
-//		System.out.println("Time taken "+time+"milisecs");
-//				
-
-		
+	public static int max(int x,int y){
+		return x>y?x:y;
 	}
-
 }
