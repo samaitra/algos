14ce3df5c5bab405066a9e46972a168a74e93a87
Saikat Maitra <saikat.maitra@gmail.com>
2012-12-24 01:23:35 +0530

    commiting string reduction changes

--- src/StringReduction.java
+++ src/StringReduction.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.Scanner;
 
 
@@ -20,27 +21,48 @@ public class StringReduction {
 			String s= in.next();
 		    String x=""; 
 		    String a="";
-		    for(int j=0;i<s.length();j++){
-			  if(j>0){
-                 if(s.charAt(j)!=s.charAt(j+1))
-                     x=x+getchar(s.charAt(j),s.charAt(j+1));   
-                 else{
-                        int k=j;
-                        while(k>0 && x.charAt(k)<x.charAt(k-1)){
-                                a=a+getchar(x.charAt(k)<x.charAt(k-1));
-                                j--;
+		    for(int j=0;j<s.length()-1;j++){
+			  System.out.println("initial string "+s);
+                 if(s.charAt(j)!=s.charAt(j+1)){
+                	 char d = getchar(s.charAt(j),s.charAt(j+1)); 
+                	 String xx = s.substring((s.indexOf(s.charAt(j+1))+1));
+                	 s=d+xx;  
+                		
+                	     
+                  	 	int k=j;
+                        System.out.println("k ==="+k);
+                  	 	while(k>0 && s.charAt(k)!=s.charAt(k-1)){
+                        	d = getchar(s.charAt(k),s.charAt(k-1)); 
+                        	xx = s.substring((s.indexOf(s.charAt(k-1))+1));
+                       	 	s=d+xx;  
+                       	    k--;
                         }
+                        
+                        System.out.println("s=============="+ s);
                 }
-        }
+        
 		}
-		
+		   
 	}
-
 
 }
 
 public static char getchar(char x,char y){
-	
+
+    if(x=='a' && y=='b')
+        return 'c';
+    else if(x=='b' && y=='c')
+        return 'a';
+    else if(x=='c' && y=='a')
+        return 'b';
+    else if(x=='a' && y=='c')
+        return 'b';
+    else if (x=='b' && y=='a')
+    	return 'c';
+    else if (x=='c' && y=='b')
+        return 'a';
+	return 'z';
+
 
 
 }
