3c96d12ba7fc884b004560e36a3c5405ee8adfa1
Saikat Maitra <saikat.maitra@gmail.com>
2012-12-25 22:43:38 +0530

    changes for string reduction

--- src/StringReduction.java
+++ src/StringReduction.java
@@ -5,21 +5,16 @@ import java.util.Scanner;
 
 public class StringReduction {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
-
+	
 		int n;
-		File f = new File("./StringReduction/input00.txt");
+		File f = new File("./StringReduction/input01.txt");
 		Scanner in = new Scanner(f);
 		//Scanner in = new Scanner(System.in);
 		n = in.nextInt();
 		
 		for(int i=0;i<n;i++){
 			String s= in.next();
-			//System.out.println("initial string "+s);
 			
 			for(int j=0;j<s.length()-1;j++){
 			  
@@ -27,30 +22,22 @@ public class StringReduction {
                 	 char d = getchar(s.charAt(j),s.charAt(j+1)); 
                 	 String xx = s.substring((s.indexOf(s.charAt(j+1))+1));
                 	 s=d+xx;  
-                     		//System.out.println("s === "+s);
-                	 		for(int k=j;k>1;k--){
-                	 		if (s.charAt(k)!=s.charAt(k-1)){
-                               	 	
-                	 	    d = getchar(s.charAt(k),s.charAt(k-1)); 
-                        	xx = s.substring((s.indexOf(s.charAt(k-2))+1));
-                       	 	//System.out.println("d === "+d);
-                       	 	//System.out.println("xx === "+xx);
-                    	 	
-                        	s=d+xx;  
+                	 		for(int k=0;k<s.length()-1;k++){
+                	 		
+                	 		  if (s.charAt(k)!=s.charAt(k+1)){
+                	 			char dy = getchar(s.charAt(k),s.charAt(k+1)); 
+                	 			String xy = s.substring((s.indexOf(s.charAt(k+1))+1));
+                	 			s=dy+xy;
+                	 			
                 	 		}
-                	 		//System.out.println("s === "+s);
                 	 		
                 	 		}
                         }
-                        
-                        
                 }
-		    System.out.println(s.length());
+		    System.out.println(s);
 		}
 		   
 	}
-
-
 
 public static char getchar(char x,char y){
 
