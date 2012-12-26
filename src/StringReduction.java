8705c4591351fc2217e814dc4176ebaf6e1f261d
Saikat Maitra <saikat.maitra@gmail.com>
2012-12-27 00:40:01 +0530

    updated files

--- src/StringReduction.java
+++ src/StringReduction.java
@@ -18,14 +18,37 @@ public class StringReduction {
 			//System.out.println(s);
 			  while(j<s.length()-1){
                      if (s.charAt(j)!=s.charAt(j+1)){
-                	 
-                     char d = getchar(s.charAt(j),s.charAt(j+1)); 
+                	 char d = getchar(s.charAt(j),s.charAt(j+1)); 
                 	 String before = s.substring(0,j);
                 	 //System.out.println("before "+before);
                 	 String after = s.substring(j+2,s.length());
                 	 //System.out.println("d "+d);
                 	 //System.out.println("after "+after);
-                	 s=before+d+after;  
+                	 String sx = before+d+after;
+                	 String sy = "";
+                	 int k=s.length()-2;
+                	 while (k>=0){
+                		 if(s.charAt(k)!=s.charAt(k+1)){
+                			 char d1 = getchar(s.charAt(k),s.charAt(k+1));
+                			 String before1 = s.substring(0,k);
+                			 //System.out.println("before1 "+before1);
+                			 String after1 = s.substring(k+2,s.length());
+                			 //System.out.println("d1 "+d1);
+                			 //System.out.println("after1 "+after1);
+                			 k=s.length()-2;
+                			 sy = before1+d1+after1;
+                			 break;
+                		 }else{
+                			 k--;
+                		 }
+                		 
+                	 }
+                	 if(sx.length()>sy.length()){
+                		 s=sx;
+                	 }else{
+                		 s=sy;
+                	 }
+                	 
                 	 //System.out.println("s == "+s);
                 	 j=0;
                      
