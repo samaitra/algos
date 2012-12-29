06cdd98eb09d005dbb39b8e6d18c9f9fe7b1e5be
Saikat Maitra <saikat.maitra@gmail.com>
2012-12-29 13:11:02 +0530

    updated files

--- src/StringReduction.java
+++ src/StringReduction.java
@@ -14,53 +14,106 @@ public class StringReduction {
 		
 		for(int i=0;i<n;i++){
 			String s = in.next();
-			int j=0;
-			//System.out.println(s);
-			  while(j<s.length()-1){
-                     if (s.charAt(j)!=s.charAt(j+1)){
-                	 char d = getchar(s.charAt(j),s.charAt(j+1)); 
-                	 String before = s.substring(0,j);
-                	 //System.out.println("before "+before);
-                	 String after = s.substring(j+2,s.length());
-                	 //System.out.println("d "+d);
-                	 //System.out.println("after "+after);
-                	 String sx = before+d+after;
-                	 String sy = "";
-                	 int k=s.length()-2;
-                	 while (k>=0){
-                		 if(s.charAt(k)!=s.charAt(k+1)){
-                			 char d1 = getchar(s.charAt(k),s.charAt(k+1));
-                			 String before1 = s.substring(0,k);
-                			 //System.out.println("before1 "+before1);
-                			 String after1 = s.substring(k+2,s.length());
-                			 //System.out.println("d1 "+d1);
-                			 //System.out.println("after1 "+after1);
-                			 k=s.length()-2;
-                			 sy = before1+d1+after1;
-                			 break;
-                		 }else{
-                			 k--;
-                		 }
-                		 
-                	 }
-                	 if(sx.length()>sy.length()){
-                		 s=sx;
-                	 }else{
-                		 s=sy;
-                	 }
-                	 
-                	 //System.out.println("s == "+s);
-                	 j=0;
-                     
-                     }else{
-                    	 j++;
-                    }
-                }
-		    System.out.println(s.length());
-		}
-		 in.close();  
-	}
+			int countA=0;
+            int countB=0;
+            int countC=0;
+            int k=0;
 
+             while(k<s.length()){
+                if(s.charAt(k)=='a')
+                    countA++;
+                else if (s.charAt(k)=='b')
+                    countB++;
+                else if (s.charAt(k)=='c')
+                    countC++;
+                k++;
+
+             }
+             int j=0;
+            while(j<(s.length()-1)){
+              char x = getReductionChar(countA,countB,countC);
+                //System.out.println("char to reduce   "+x);
+                //System.out.println("j "+j);
+                if(x==s.charAt(j) || x==s.charAt(s.length()-1) ||x==s.charAt(0)){
+                    if (s.charAt(j)!=s.charAt(j+1)){
+                      //System.out.println("I am here");
+                     char d = getchar(s.charAt(j),s.charAt(j+1));
+
+                     if(s.charAt(j)=='a'){
+                         countA--;
+                     }else if(s.charAt(j)=='b'){
+                         countB--;
+                     }else if (s.charAt(j)=='c'){
+                         countC--;
+                     }
+
+                        if(s.charAt(j+1)=='a'){
+                            countA--;
+                        }else if(s.charAt(j+1)=='b'){
+                            countB--;
+                        }else if (s.charAt(j+1)=='c'){
+                            countC--;
+                        }
+
+                        if(d=='a'){
+                            countA++;
+                        }else if(d=='b'){
+                            countB++;
+                        }else if (d=='c'){
+                            countC++;
+                        }
+
+                     String before = s.substring(0,j);
+                	 String after = s.substring(j+2,s.length());
+                	 s = before+d+after;
+                     //System.out.println("In if "+s);
+                     j=0;
+                    } else if(j>0){
+                       if (s.charAt(j-1)!=s.charAt(j)){
+
+                        char d = getchar(s.charAt(j),s.charAt(j-1));
+
+                        if(s.charAt(j)=='a'){
+                            countA--;
+                        }else if(s.charAt(j)=='b'){
+                            countB--;
+                        }else if (s.charAt(j)=='c'){
+                            countC--;
+                        }
+
+                        if(s.charAt(j-1)=='a'){
+                            countA--;
+                        }else if(s.charAt(j-1)=='b'){
+                            countB--;
+                        }else if (s.charAt(j-1)=='c'){
+                            countC--;
+                        }
+
+                        if(d=='a'){
+                            countA++;
+                        }else if(d=='b'){
+                            countB++;
+                        }else if (d=='c'){
+                            countC++;
+                        }
+
+                        String before = s.substring(0,j-1);
+                        String after = s.substring(j+1,s.length());
+                        s = before+d+after;
+                        //System.out.println("In else "+s);
+                        j=0;
+                    }
+                   }
+                  }
+                  j++;
+                }
+             if(s.length()==2 && s.charAt(0)!=s.charAt(1)){
+                 char d = getchar(s.charAt(0),s.charAt(1));
+                    s = String.valueOf(d);
+             }
+             System.out.println(s.length());
+        }
+    }
 public static char getchar(char x,char y){
 
     if(x=='a' && y=='b')
@@ -77,7 +130,17 @@ public static char getchar(char x,char y){
         return 'a';
 	return 'z';
 
-
-
 }
+
+    public static char getReductionChar(int a,int b,int c){
+        if(a>=b && a>=c)
+            return 'a';
+        else if(b>=c && b>=a)
+            return 'b';
+        else
+            return 'c';
+    }
+
+
+
 }
