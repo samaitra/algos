c0e967dbea99dfb53f73bad6dd37fed7e325089e
Saikat Maitra <saikat.maitra@gmail.com>
2013-03-30 00:04:23 +0530

    suffix tree

--- src/FindStrings.java
+++ src/FindStrings.java
@@ -3,48 +3,56 @@ import java.util.*;
 
 
 public class FindStrings {
+    static HashMap<String,Integer> map  = new HashMap<>();
 
     public static void main(String[] args) {
 
-        Set<String> stringSet = new HashSet<String>();
-
         try{
-            Scanner in = new Scanner(new FileReader("FindStrings/input.txt"));
+            Scanner in = new Scanner(new FileReader("FindStrings/input05.txt"));
             int n = in.nextInt();
-            System.out.println("n "+n);
-
             for (int i=0;i<n;i++){
 
                 String str = in.next();
-                findSubString(stringSet,str);
+                getSuffixArray(str);
+
 
             }
+            String a[] = new String[map.size()];
+            int c=0;
+            for(Map.Entry<String,Integer> entry : map.entrySet()){
+               a[c] = entry.getKey();
+               c++;
+            }
+            Arrays.sort(a);
 
             int q = in.nextInt();
             for(int i=0;i<q;i++){
                 int k = in.nextInt();
-
+                int len = a.length;
+                if(len<k){
+                    System.out.println("INVALID");
+                }else{
+                System.out.println(a[k]);
+                }
             }
 
         }catch(Exception e){
             e.printStackTrace();
-            System.out.println("INVALID");
+
 
         }
 
-        stringSet.clear();
+
     }
 
-    private static String[] getSuffixArray(String str){
+    private static void getSuffixArray(String str){
         int n = str.length();
-        String[] suffixes = new String[n];
 
         for(int i=0;i<n;i++){
-            suffixes[i]=str.substring(i);
-        }
-        Arrays.sort(suffixes);
-        return suffixes;
+            map.put(str.substring(0,i),0);
+            map.put(str.substring(i,n),0);
 
+        }
 
     }
 
---
+++ src/SuffixTree.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: saikat
+ * Date: 30/03/13
+ * Time: 12:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SuffixTree {
+
+
+
+
+}
