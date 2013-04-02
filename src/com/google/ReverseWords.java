06ac64307a2c75b00591f606fae5aa12d3b9b04f
Saikat Maitra <saikat.maitra@gmail.com>
2013-04-02 20:58:42 +0530

    reverse words

--- src/com/google/ReverseWords.java
+++ src/com/google/ReverseWords.java
@@ -1,5 +1,9 @@
 package com.google;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * User: saikat
@@ -8,4 +12,24 @@ package com.google;
  * To change this template use File | Settings | File Templates.
  */
 public class ReverseWords {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader b = new BufferedReader(new FileReader("ReverseWords/B-large-practice.in"));
+        int n = Integer.parseInt(b.readLine());
+        for(int i=0;i<n;i++){
+            String line = b.readLine();
+            //System.out.println(line);
+            String[] a = line.split(" ");
+            StringBuilder s = new StringBuilder();
+            for(int j=a.length-1;j>=0;j--){
+                if(j==0)
+                s.append(a[j]);
+                else
+                    s.append(a[j]+" ");
+            }
+            System.out.println("Case #"+(i+1)+": "+s.toString());
+        }
+
+    }
+
 }
