ad4de91bfb2b4bd2706c53b9fc385dfce26f292a
Saikat Maitra <saikat.maitra@gmail.com>
2013-06-15 18:27:32 +0530

    kc

--- src/com/hr/KingdomConnectivity.java
+++ src/com/hr/KingdomConnectivity.java
@@ -18,10 +18,12 @@ import static java.lang.System.out;
 public class KingdomConnectivity {
 
   int N,M;
-  int MOD = 1000000000;
-  boolean[] visited,visited2;
-  Stack<City> visitedCities = new Stack<City>();
-  public static void main(String[] args) throws Exception{
+  static int MOD = 1000000000;
+  static boolean[] visited,visited2;
+  static Stack<City> visitedCities = new Stack<City>();
+  static City fc,lc;
+  static Status s = new Status();
+    public static void main(String[] args) throws Exception{
       Scanner in = new Scanner(new FileReader("kc/in"));
 
       int N = in.nextInt();
@@ -34,8 +36,8 @@ public class KingdomConnectivity {
           cities[i]=c;
       }
 
-      City fc = cities[1];
-      City lc = cities[N];
+       fc = cities[1];
+       lc = cities[N];
 
       for(int i=0;i<M;i++){
           int from = in.nextInt();
@@ -43,24 +45,104 @@ public class KingdomConnectivity {
           cities[from].neighbours.add(cities[to]);
       }
 
-       countPath(fc);
-
+       int c = countPath(fc);
+        if(s.infinity){
+           System.out.println("INFINITE PATHS");
+       }else{
+           System.out.println(c);
+       }
   }
 
-    private static void countPath(City fc) {
+    private static int countPath(City city) {
 
+        if(city.pathCount!=0){
+            return (city.pathCount);
+        }
 
+        if(city.visited){
+            for(City c : visitedCities){
+              boolean reachable = isReacheable(c,lc);
+              if(reachable){
+                  s.infinity = true;
+                  //return -1;
+
+              }
+                if(c == city){
+                    break;
+                }
+            }
+
+            return 0;
+        }
+
+        city.visited = true;
+        visitedCities.push(city);
+        Set<City> neighbours = city.neighbours;
+
+        int totalCount = 0;
+        if(city==lc){
+           totalCount=1;
+        }
+        for(City n : neighbours){
+            totalCount = (totalCount + countPath(n)) % MOD;
+        }
+
+        visitedCities.pop();
+        city.visited = false;
+        city.pathCount = totalCount;
+
+        return totalCount;
 
 
 
 
     }
 
-}
+    private static boolean isReacheable(City city, City to) {
+
+        if(city==to){
+           return true;
+       }
+       if(city.visited2){
+           return false;
+       }
+
+        boolean result = city.reachable;
+
+        city.visited2 = true;
+
+       boolean anyReachable = false;
+        for(City neighbour : city.neighbours){
+           boolean reachable = isReacheable(neighbour, to);
+
+           if(reachable){
+                   anyReachable = true;
+                   break;
+               }
+           }
+
+        city.visited2 = false;
+        city.reachable = anyReachable?true:false;
+        return anyReachable;
+       }
+
+
+
+
+    }
+ class Status{
+     boolean infinity=false;
+
+ }
+
  class City{
      int id;
      int pathCount;
      Set<City> neighbours;
+     boolean visited;
+     boolean visited2;
+     boolean reachable;
+
      City(int id){
        this.id = id;
        pathCount = 0;
