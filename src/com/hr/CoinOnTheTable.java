8118ee4fd339189cc141fb358c6b234c905f189d
Saikat Maitra <saikat.maitra@gmail.com>
2013-06-17 00:28:38 +0530

    COTT

--- src/com/hr/CoinOnTheTable.java
+++ src/com/hr/CoinOnTheTable.java
@@ -12,9 +12,10 @@ import static java.lang.System.*;
  * To change this template use File | Settings | File Templates.
  */
 public class CoinOnTheTable {
+    static int count=0;
     public static void main(String[] args)throws Exception{
 
-    Scanner in = new Scanner(new FileReader("coin/in"));
+    Scanner in = new Scanner(new FileReader("coin/in2"));
     int n = in.nextInt();
     int m = in.nextInt();
     int k = in.nextInt();
@@ -28,10 +29,56 @@ public class CoinOnTheTable {
             table[i][j] = chars[j];
         }
     }
-   out.println(table[1][1]);
+
+    count = findStep(table, 0, 0, k);
+    if(count==k){
+        out.println(0);
+    }else{
+       int step = changeStep(table, 0, 0, k);
+       out.println(step);
+    }
+    }
+
+    public static int changeStep(char[][]table,int i,int j,int k){
+        int step=0;
 
 
 
 
-}
+
+
+
+        return step;
+    }
+
+
+
+
+    public static int findStep(char[][]table,int i,int j,int k){
+
+       char c = table[i][j];
+       System.out.println("c = " + c);
+
+       if(c=='*'){
+           return 1;
+       }
+       else if(c=='U'){
+          findStep(table,i-1,j,k);
+          count++;
+       }
+       else if(c=='L'){
+           findStep(table,i,j-1,k);
+           count++;
+       }
+       else if(c=='D'){
+           findStep(table,i+1,j,k);
+           count++;
+       }
+       else if(c=='R'){
+           findStep(table,i,j+1,k);
+           count++;
+       }
+       return count;
+    }
+
 }
