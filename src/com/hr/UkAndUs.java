deadbb37529f84df495e48e26644a05a2cedae92
Saikat Maitra <saikat.maitra@gmail.com>
2013-07-02 22:44:51 +0530

    uk and us

--- src/com/hr/UkAndUs.java
+++ src/com/hr/UkAndUs.java
@@ -2,6 +2,7 @@ package com.hr;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,7 +16,7 @@ import java.util.regex.Pattern;
 public class UkAndUs {
 
     public static void main(String[] args) throws Exception{
-    BufferedReader bufferedReader =new BufferedReader (new FileReader("findhackerrank/in"));
+    BufferedReader bufferedReader =new BufferedReader (new InputStreamReader(System.in));
     int n = Integer.parseInt(bufferedReader.readLine());
     StringBuilder sb = new StringBuilder();
     for(int i=0;i<n;++i){
@@ -27,6 +28,16 @@ public class UkAndUs {
     for(int i=0;i<t;++i){
 
         String w = bufferedReader.readLine();
+        String x = w.substring(0,(w.length()-2));
+        Pattern p = Pattern.compile(x+"[zs]e");
+        Matcher m = p.matcher(sb.toString());
+        int c = 0;
+        while(m.find()){
+          c++;
+        }
+
+        System.out.println(c);
+
     }
 
 }
