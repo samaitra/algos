f88ed936497b0eb682cbec7664624e8b8f1c0dea
samaitra <saikat.maitra@gmail.com>
2013-07-14 16:51:58 +0530

    regex links and text

--- src/com/hr/HtmlTags.java
+++ src/com/hr/HtmlTags.java
@@ -1,5 +1,14 @@
 package com.hr;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Saikat
@@ -8,4 +17,38 @@ package com.hr;
  * To change this template use File | Settings | File Templates.
  */
 public class HtmlTags {
+
+    public static void main(String[] args) throws Exception{
+
+        //BufferedReader bufferedReader =new BufferedReader (new InputStreamReader(System.in));
+    BufferedReader bufferedReader =new BufferedReader (new FileReader("htmltags/in"));
+
+    int n = Integer.parseInt(bufferedReader.readLine());
+    StringBuilder sb = new StringBuilder();
+    for(int i=0;i<n;++i){
+        String line = bufferedReader.readLine();
+        sb.append(line);
+
+    }
+        HashSet<String> tagSet = new HashSet<String>();
+        Pattern p = Pattern.compile("</\\w+>");
+        Matcher m = p.matcher(sb.toString());
+        while(m.find()){
+         String tag = m.group();
+         tag = tag.replace("</","");
+         tag = tag.replace(">","");
+         tagSet.add(tag);
+        }
+        ArrayList<String> aList = new ArrayList<String>();
+        for(String s : tagSet){
+          aList.add(s);
+        }
+        Collections.sort(aList);
+        String line = "";
+        for(String s : aList){
+          line=line+s+";";
+        }
+        System.out.print(line.substring(0,line.length()-1));
+
+}
 }
