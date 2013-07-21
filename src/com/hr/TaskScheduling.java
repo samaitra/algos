21e8a0e2b7c52fb99826a3af4bf9031fd23419c0
Saikat Maitra <saikat.maitra@gmail.com>
2013-07-21 23:25:31 +0530

    Task scheduling

--- src/com/hr/TaskScheduling.java
+++ src/com/hr/TaskScheduling.java
@@ -15,66 +15,54 @@ public class TaskScheduling {
     public static void main(String[] args) throws Exception{
         BufferedReader bufferedReader = new BufferedReader(new FileReader("taskscheduling/in"));
         int n = Integer.parseInt(bufferedReader.readLine());
-        int d[] = new int[n];
-        int m[] = new int[n];
         for(int i=0;i<n;i++){
             String line = bufferedReader.readLine();
-            String a[] = line.split("\\s");
-            d[i] = Integer.parseInt(a[0]);
-            m[i] = Integer.parseInt(a[1]);
-            //System.out.println(d[i]+" "+m[i]);
+            String[] pairs = line.split("\\s");
+            int deadline = Integer.parseInt(pairs[0]);
+
         }
-        //System.out.println("num of elements "+n);
-        for(int i=0;i<n;i++){
-          int dx[] = new int[(i+1)];
-          int mx[] = new int[(i+1)];
 
 
-
-          System.arraycopy(d,0,dx,0,i+1);
-          System.arraycopy(m,0,mx,0,i+1);
-          // use hashmap to store mx and dx values, so that after sorting mx we can still get dx value
-          HashMap<Integer,Integer> timeMap = new HashMap<Integer,Integer>();
-          for(int k=0;k<(i+1);k++){
-             timeMap.put(mx[k],dx[k]);
-          }
-
-          Arrays.sort(mx);
-
-          int len = mx.length;
-            //System.out.println("len "+len);
-
-//            for(int j=0;j<(i+1);j++){
-//
-//                System.out.println("mx[j] before reverse sort " + mx[j] + " ");
-//
-//            }
-//            System.out.println("--------------");
-            for(int j=0;j<len/2;j++){
-
-                int temp = mx[j];
-                mx[j]= mx[len-1-j];
-                mx[len-1-j]=temp;
-
-            }
-
-
-//            for(int j=0;j<(i+1);j++){
-//
-//                System.out.println("mx[j] after reverse sort " + mx[j] + " ");
-//
-//            }
-//
-//            System.out.println("--------------");
-
-
-
-            for(int j=0;j<(i+1);j++){
-                int deadline = timeMap.get(mx[j]);
-                System.out.println("taskSlot order "+deadline+" task execution time Mi "+mx[j]+"");
-            }
-
-            System.out.println("===============");
-        }
     }
+}
+
+class Tasks{
+    int deadline;
+    int executionTime;
+    int order;
+
+    Tasks(int deadline, int executionTime) {
+        this.deadline = deadline;
+        this.executionTime = executionTime;
+    }
+
+    Tasks(int deadline, int executionTime, int order) {
+        this.deadline = deadline;
+        this.executionTime = executionTime;
+        this.order = order;
+    }
+    int getDeadline() {
+        return deadline;
+    }
+
+    void setDeadline(int deadline) {
+        this.deadline = deadline;
+    }
+
+    int getExecutionTime() {
+        return executionTime;
+    }
+
+    void setExecutionTime(int executionTime) {
+        this.executionTime = executionTime;
+    }
+
+    int getOrder() {
+        return order;
+    }
+
+    void setOrder(int order) {
+        this.order = order;
+    }
+
 }
