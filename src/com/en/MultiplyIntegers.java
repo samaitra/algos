360f9dc55848006a7361f6cc5ec021cdd0d6392c
Saikat Maitra <saikat.maitra@gmail.com>
2013-07-29 23:20:06 +0530

    Evernote challenges

--- src/com/en/MultiplyIntegers.java
+++ src/com/en/MultiplyIntegers.java
@@ -12,25 +12,37 @@ import java.util.Scanner;
  * Time: 9:45 PM
  * To change this template use File | Settings | File Templates.
  */
-public class IntegerMultiply {
+public class MultiplyIntegers {
     public static void main(String args[]) throws Exception{
-      //BufferedReader br = new BufferedReader(new FileReader("multiplyintegers/in"));
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedReader br = new BufferedReader(new FileReader("multiplyintegers/in"));
+      //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
     	int globalVal = 1;
     	int[] a = new int[N];
-    	for(int i=0;i<N;i++){
+    	int numZeros=0;
+        for(int i=0;i<N;i++){
     		int num = Integer.parseInt(br.readLine());
     		a[i] = num;
-            if(num!=0){
-    		globalVal = globalVal * num;
+
+            if(num==0){
+                numZeros++;
+            }
+
+            if(numZeros>=2){
+              globalVal=0;
+            }
+            else if(num!=0){
+    		  globalVal = globalVal * num;
             }
     	}
     	for(int i=0;i<N;i++){
     		if(a[i]==0){
                 System.out.println(globalVal);
-            }else{
+            }else if(a[i]!=0 && numZeros==1){
+                System.out.println(0);
+            }
+            else{
             System.out.println(globalVal/a[i]);
             }
           }
