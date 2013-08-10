ac1b0e3039a21d5cea7e8cf0ac537b632aa1196f
Saikat Maitra <saikat.maitra@gmail.com>
2013-08-10 15:28:43 +0530

    SwapWithoutTemp

---
+++ src/com/gen/SwapWithoutTemp.java
@@ -0,0 +1,30 @@
+package com.gen;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: saikat
+ * Date: 8/10/13
+ * Time: 3:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SwapWithoutTemp {
+
+    public static void main(String[] args){
+
+        //using XOR operation;
+        int a = 2;
+        int b = 4;
+        a = a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("a "+a);
+        System.out.println("b "+b);
+
+        //using arithmatic operation
+        a = a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a "+a);
+        System.out.println("b "+b);
+    }
+}
