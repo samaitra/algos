5018404313e34fab904e0f853800880dff4ee59f
Saikat Maitra <saikat.maitra@gmail.com>
2013-08-21 00:26:17 +0530

    Letter order

--- src/com/gen/FindOrderOfLetter.java
+++ src/com/gen/FindOrderOfLetter.java
@@ -1,5 +1,7 @@
 package com.gen;
 
+import java.util.HashMap;
+
 /**
  * Created with IntelliJ IDEA.
  * User: saikat
@@ -17,13 +19,72 @@ public class FindOrderOfLetter {
             System.out.println( "NOT POSSIBLE");
         }else {
             for(char c : letters){
-                System.out.println(c + ",");
+                System.out.print(c + ",");
             }
         }
     }
 
     private static char[] getOrderedLetter(String[] words) {
+        HashMap<Character,Integer> chars = new HashMap<Character,Integer>();
+        for(String word : words){
 
+            for(int i=0;i<word.length();i++){
+              if(!chars.keySet().contains(word.charAt(i))){
+                  chars.put(word.charAt(i),chars.size());
+              }
+            }
+
+
+        }
+
+        boolean[][] adjacencyList = new boolean[chars.size()][chars.size()];
+
+        for(int i=0;i<words.length-1;i++){
+            for(int j=i+1;j<words.length;j++){
+                String prevWord = words[i];
+                String nextWord = words[j];
+
+                for(int k=0;k<Math.min(prevWord.length(),nextWord.length());k++){
+                    char prevChar = prevWord.charAt(k);
+                    char nextChar = nextWord.charAt(k);
+                    if(prevChar!=nextChar){
+                        adjacencyList[chars.get(prevChar)][chars.get(nextChar)] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        char[] result = new char[chars.size()];
+        int resultIndex = 0;
+        while (!chars.isEmpty()){
+            char lowChar = ' ';
+            for(Character nextCharacter : chars.keySet()){
+                int nextIndex = chars.get(nextCharacter);
+                boolean lowest = true;
+                for(Character prevCharacter : chars.keySet()){
+                    int prevIndex = chars.get(prevCharacter);
+                    if(adjacencyList[prevIndex][nextIndex]){
+                        lowest = false;
+                        break;
+                    }
+                }
+
+                if(lowest){
+                    lowChar = nextCharacter;
+                    result[resultIndex++] = lowChar;
+                    break;
+                }
+
+            }
+            if(lowChar == ' '){
+                return null;
+            }else{
+                chars.remove(lowChar);
+                lowChar = ' ';
+            }
+        }
+        return result;
 
     }
 }
