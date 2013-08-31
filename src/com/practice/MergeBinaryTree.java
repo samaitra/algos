dc99780a2f8462e57ab5f70d94538ae9eec2a9fc
Saikat Maitra <saikat.maitra@gmail.com>
2013-08-31 23:26:38 +0530

    Merge Binary tree and create balance binary tree

--- src/com/practice/MergeBinaryTree.java
+++ src/com/practice/MergeBinaryTree.java
@@ -8,13 +8,100 @@ import java.util.regex.*;
 
 class MergeBinaryTree{
 	
-	
-	public static void main(String[] args){
-       Node a
+	static LinkedList<Node> nodeList = new LinkedList<>();
 
 
+    public static void main(String[] args){
+       Node a = new Node(90);
+       Node b = new Node(70);
+       Node c = new Node(110);
 
-	}
+       a.left = b;
+       a.right = c;
+
+       Node x = new Node(60);
+       Node y = new Node(5);
+       Node z = new Node(800);
+
+       x.left = y;
+       x.right = z;
+
+      createLinkedList(a,x);
+      Collections.sort(nodeList, new DataComparable());
+      if((nodeList.size()%2==0)){
+        nodeList.remove(nodeList.size()-1);
+      }
+       for(Node n : nodeList){
+          System.out.print(n.data + " ");
+       }
+       System.out.println();
+       Node root = createTree(nodeList);
+       printTree(root);
+
+
+    }
+
+    private static void printTree(Node n){
+        if(n==null){
+            return;
+        }
+        System.out.println(n.data+" ");
+        printTree(n.left);
+        printTree(n.right);
+    }
+
+    public static Node createTree(LinkedList<Node> list){
+
+      Node root = list.get(list.size()/2);
+      System.out.println("root "+root.data);
+      list.remove(root);
+
+      addToTree(root,list);
+      return root;
+    }
+
+    public static void addToTree(Node root, LinkedList<Node> linkedList){
+        for(Node n : linkedList){
+            addToTree(root, n.data);
+        }
+    }
+    public static void addToTree(Node root,int n1){
+        Node n = new Node(n1);
+        if(n.data<root.data){
+        if(root.left==null){
+            root.left = n;
+        }else{
+            addToTree(root.left,n1);
+        }
+
+        }else{
+            if(root.right==null){
+                root.right = n;
+            }else{
+                addToTree(root.right,n1);
+            }
+        }
+
+
+    }
+
+    public static void createLinkedList(Node root1, Node root2){
+
+        createList(root1, nodeList);
+        createList(root2, nodeList);
+
+    }
+
+    public static void createList(Node root,LinkedList<Node> nodeList){
+
+        if(root==null){
+            return;
+        }
+        nodeList.add(root);
+        createList(root.left,nodeList);
+        createList(root.right,nodeList);
+
+    }
 
     static class Node{
 
@@ -27,6 +114,20 @@ class MergeBinaryTree{
         }
 
     }
-	
+
+    static class DataComparable implements Comparator<Node>{
+
+        public int compare(Node o1, Node o2) {
+            Node n1 = (Node) o1;
+            Node n2 = (Node) o2;
+
+            Integer d1 = n1.data;
+            Integer d2 = n2.data;
+            return d1.compareTo(d2);
+        }
+    }
+
+
 
 }
+
