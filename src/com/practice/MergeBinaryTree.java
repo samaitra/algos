73cff27a05e45dae11e88e821307dd913d272785
Saikat Maitra <saikat.maitra@gmail.com>
2013-08-31 19:35:25 +0530

    Merge binary tree

---
+++ src/com/practice/MergeBinaryTree.java
@@ -0,0 +1,32 @@
+package com.practice;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class MergeBinaryTree{
+	
+	
+	public static void main(String[] args){
+       Node a
+
+
+
+	}
+
+    static class Node{
+
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+        }
+
+    }
+	
+
+}
