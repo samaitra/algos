71c9e336d578b8b6e099cfe88a73caa075b7c454
Saikat Maitra <saikat.maitra@gmail.com>
2013-09-02 16:17:33 +0530

    Bits manipulation

---
+++ src/com/practice/Bits.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: saikat
+ * Date: 02/09/13
+ * Time: 10:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bits {
+
+    public static void main(String[] args){
+
+    }
+
+}
