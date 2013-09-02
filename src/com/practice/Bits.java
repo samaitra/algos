6f6ca4be3a9391c18513d107423f8803b0c22939
Saikat Maitra <saikat.maitra@gmail.com>
2013-09-02 16:52:38 +0530

    Bits manipulation

--- src/com/practice/Bits.java
+++ src/com/practice/Bits.java
@@ -1,5 +1,7 @@
 package com.practice;
 
+import java.util.BitSet;
+
 /**
  * Created with IntelliJ IDEA.
  * User: saikat
@@ -10,7 +12,15 @@ package com.practice;
 public class Bits {
 
     public static void main(String[] args){
+        int x = Integer.parseInt("10000000000",2);
+        int y = Integer.parseInt("10101",2);
+        int i = 2, j = 6;
+        Integer num = F(x,y,i,j);
+        System.out.print(Integer.toBinaryString(num));
 
     }
-
+    public static int F(int x, int y, int i, int j){
+        int mask = (-1<<(j+1)) | (-1>>>(32-i));
+        return (mask & x ) | (y<<i);
+    }
 }
