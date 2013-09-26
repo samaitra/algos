1f55f8d5a244dc625d66105bfb0fd07932871c40
Saikat Maitra <saikat.maitra@gmail.com>
2013-09-27 01:59:33 +0530

    median

--- src/com/hr/Median.java
+++ src/com/hr/Median.java
@@ -1,110 +1,138 @@
 package com.hr;
 
-import java.io.File;
+import java.io.FileReader;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Median {
 
-	public static void main(String[] args) throws Exception {
-		
-//		Date starttime = new Date();
-//		File f =  new File("./Median/input00.txt");
-//		
-		Scanner in = new Scanner(System.in);
-		//Scanner in = new Scanner(f);
+     PriorityQueue<Integer> upperQ;
+     PriorityQueue<Integer> lowerQ;
+     ArrayList<Integer> l;
+    public Median(int n){
+        lowerQ = new PriorityQueue<Integer>(n, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return -o1.compareTo(o2);
+            }
+        });
+
+        upperQ = new PriorityQueue<Integer>();
+        upperQ.add(Integer.MAX_VALUE);
+        lowerQ.add(Integer.MIN_VALUE);
+        l = new ArrayList<>();
+    }
+
+    public double getMedianAfterAdd(Integer num){
+        l.add(num);
+
+        if(num>=upperQ.peek())
+            upperQ.add(num);
+        else
+            lowerQ.add(num);
+
+        if(upperQ.size()-lowerQ.size()==2)
+            lowerQ.add((upperQ.poll()));
+        else if(lowerQ.size()-upperQ.size()==2)
+            upperQ.add(lowerQ.poll());
+
+        if(upperQ.size()==lowerQ.size()){
+                 long sum=0;
+                 sum += upperQ.peek();
+                 sum += lowerQ.peek();
+
+            return sum/2.0;
+        }
+        else if(upperQ.size()>lowerQ.size()){
+            return upperQ.peek();
+        }
+        else{
+
+            return lowerQ.peek();
+        }
+    }
+
+    public double getMedianAfterRemove(Integer num){
+
+        if(l.contains(num)){
+            l.remove(num);
+        }
+
+
+        if(upperQ.contains(num))
+            upperQ.remove(num);
+        else if(lowerQ.contains(num))
+            lowerQ.remove(num);
+
+        if(upperQ.size()-lowerQ.size()==2)
+            lowerQ.add((upperQ.poll()));
+        else if(lowerQ.size()-upperQ.size()==2)
+            upperQ.add(lowerQ.poll());
+
+        if(upperQ.size()==lowerQ.size()){
+
+            long sum = 0;
+            sum += upperQ.peek();
+            sum += lowerQ.peek();
+            return sum/2.0;
+
+        }
+        else if(upperQ.size()>lowerQ.size()){
+            return upperQ.peek();
+        }
+        else{
+            return lowerQ.peek();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+		//Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(new FileReader("in"));
 		int N;
 		N = in.nextInt();
-	
-		String s[] = new String[N];
-		int x[] = new int[N];
-			
+        Median m = new Median(N);
+
+        String s[] = new String[N];
+	    int x[] = new int[N];
+
 		for(int i=0; i<N; i++){
 			s[i] = in.next();
 			x[i] = in.nextInt();
 		}
-	    float median; 
-		ArrayList<Integer> alist = new ArrayList<Integer>();
 		for (int i=0;i<N;i++){
-			//System.out.println("s[i]  "+s[i]+"  x[i]  "+x[i]);
-			
+
 			if(s[i].equals("a")){
 	    		 Integer val = x[i];
-	    		 alist.add(val);
-	    		 printMedian(alist);
-	 	    	 
-	    	 }else if(s[i].equals("r")){
-	    		 Integer o = x[i];
-	 	    	 if(find(alist,o)){
-	    		 alist.remove(o);
-	    		 printMedian(alist);
-	 	    	   if(alist.size()==0){
-	    			   System.out.println("Wrong!");
-	    		   }
-	 	    	 }else{
-	 	    		 System.out.println("Wrong!");
-	 	    	}
-	    	 } 
-			 
-	    	 
-	        }
-	   
-//		Date endtime = new Date();
-//		long time=endtime.getTime()-starttime.getTime();
-//		System.out.println("Time taken "+time+"milisecs");
-//				
-		
-	}
-	
-	public static void printMedian(ArrayList<Integer> alist){
-		 Collections.sort(alist);
-		 
-		    if (alist.size() % 2 == 1)
-			System.out.println(alist.get((alist.size()+1)/2-1));
-		    else
-		    {
-		    try{	
-			double lower = alist.get(alist.size()/2-1);
-			double upper = alist.get(alist.size()/2);
-		    
-			double median = ((lower + upper) / 2.0);
-			System.out.println(new DecimalFormat("#.##").format(median));
-		    }catch(Exception e){
-		    	//e.printStackTrace();
-		    }
-		    }	
-		
-		
-	}
-	public static boolean find(ArrayList<Integer> alist,Integer o){
-		
-		Collections.sort(alist);
-		
-		if (alist.size() == 0) {
-		      return false;
-		    }
-		
-		    int low = 0;
-		    int high = alist.size()-1;
+	    		 System.out.println(new DecimalFormat("#.##").format(m.getMedianAfterAdd(val)));
 
-		    while(low <= high) {
-		      int middle = (low+high) /2; 
-		      if (o > alist.get(middle)){
-		        low = middle +1;
-		      } else if (o < alist.get(middle)){
-		        high = middle -1;
-		      } else { // The element has been found
-		        return true; 
-		      }
-		    }
-		    return false;
-		  }
-	
+	    	 }else if(s[i].equals("r")){
+	    		 Integer val = x[i];
+                 if(m.l.contains(val)){
+                    if(m.l.size()<=1){
+                        System.out.println("Wrong!");
+
+                    }else{
+
+                        System.out.println(new DecimalFormat("#.##").format(m.getMedianAfterRemove(val)));
+
+                    }
+                }else{
+                    System.out.println("Wrong!");
+                }
+	    	 }
+
+
+	        }
+
+
+	}
+
+
+
+
 }
 
 
