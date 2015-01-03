3b4ddf97eda2bc6b2d71f6983f926646fd851cd3
Saikat Maitra <saikat.maitra@gmail.com>
2015-01-03 21:40:20 +0530

    RegexMatcher

--- src/com/careercup/RelocateArray.java
+++ src/com/careercup/RelocateArray.java
@@ -2,6 +2,13 @@ package com.careercup;
 
 /**
  * Created by saikat on 31/12/14.
+ * WAP to modify the array such that arr[I] = arr[arr[I]].
+ * Do this in place i.e. with out using additional memory.
+
+ * example : if a = {2,3,1,0}
+ * o/p = a = {1,0,3,2}
+
+ * Note : The array contains 0 to n-1 integers.
  */
 public class RelocateArray {
 
